cdddacc8cb998cfdcbf861506e64f48717956763
IsaevRail <isaevrail@gmail.com>
2016-06-12 23:24:21 +0300

    first commit

---
+++ gulp/tasks/copy.fonts.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = function () {
+  $.gulp.task('copy.fonts', function () {
+    return $.gulp.src('./app/fonts/*.*')
+      .pipe($.gulp.dest($.config.root + '/fonts'))
+  })
+};
